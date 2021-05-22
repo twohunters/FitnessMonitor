216b44e26519d2ab05e46189289dac2dc6c63c00
Jason Anderson <jas.nanders.n@gmail.com>
2021-05-21 23:58:52 -0700

    built html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-    // index.html
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("/exercise", (req, res) => {
-    // exercise.html
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-    // stats.html
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
