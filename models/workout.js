46dde7eb992efc6d76670bb45b8c46c7ed1412af
Jason Anderson <jas.nanders.n@gmail.com>
2021-05-21 23:47:53 -0700

    built workout model

--- models/workout.js
+++ models/workout.js
@@ -2,9 +2,54 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    // create new workout schema
-});
+    day: {
+        type: Date,
+        default: Date.now,
+    },
+    exercise: [{
+        type: {
+            type: String,
+            trim: true,
+            required: [true, "Select a type of exercise"]
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: [true, "Enter the name of the workout"]
+        },
+        duration: {
+            type: Number,
+            default: 0,
+            required: [true, "Enter the duration of the workout in minutes"]
+        },
+        weight: {
+            type: Number,
+            default: 0,
+            required: [true, "Enter an amount of weight in pounds"]
+        },
+        reps: {
+            type: Number,
+            default: 0,
+            required: [true, "Enter the number of reps"]
+        },
+        sets: {
+            type: Number,
+            default: 0,
+            required: [true, "Enter the number of sets"]
+        },
+        distance: {
+            type: Number,
+            default: 0,
+            required: [true, "Enter the distance in feet"]
+        }
+    }]
+},
+    {
+        toJSON: {
+            virtuals: true
+        }
+    });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
