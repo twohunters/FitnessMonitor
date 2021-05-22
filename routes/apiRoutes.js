11525ae32753faf05bc48314111f20b9b4077b2c
Jason Anderson <jas.nanders.n@gmail.com>
2021-05-22 00:30:16 -0700

    started building api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,24 @@
 const router = require("express").Router();
-const Workout = require("../models/workout");
+const db = require("../models/workout");
 
 router.post("/api/workouts", ({ body }, res) => {
-    // create workouts
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
 });
 
 router.get("/api/workouts", (req, res) => {
-    // get workouts
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
 });
 
 router.put("/api/workouts/:id", (req, res) => {
