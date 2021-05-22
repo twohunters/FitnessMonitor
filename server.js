d115bfd5ee1ea470fb03770f249fa0757c899d7d
Jason Anderson <jas.nanders.n@gmail.com>
2021-05-21 23:27:55 -0700

    fixed syntax error in server.js

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ mongoose.connect(
     }
 );
 
-app.use(require("routes/apiRoutes.js"));
-app.use(require("routes/htmlRoutes.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on Port ${PORT}!`)
